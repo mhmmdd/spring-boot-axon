6aa5229dcca3dc5af4baa2c3009bdb93a107c712
mhmmdd <mhmmd.dd@gmail.com>
2018-03-12 09:55:55 +0300

    Initial commit

---
+++ src/main/java/org/springframework/boot/springbootaxon/SpringBootAxonApplication.java
@@ -0,0 +1,12 @@
+package org.springframework.boot.springbootaxon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAxonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAxonApplication.class, args);
+	}
+}
